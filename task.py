7eedaf451f5ce89cc542abbb8ed120b9641ab4e8
Sikarcodes <165329267+Sikarcodes@users.noreply.github.com>
2025-11-26 03:54:15 -0500

    Enhance Task class with priority functionality
    Added priority attribute and related methods to Task class.

--- task.py
+++ task.py
@@ -1,19 +1,45 @@
+# class Task():
+
+
+# 	def __init__(self, taskName: str, taskDesc: str):
+# 		self.taskName = taskName 
+# 		self.taskDesc = taskDesc 
+
+# 	def updateTaskName(self, newTaskName: str):
+# 		self.taskName = newTaskName
+
+# 	def updateTaskDesc(self, newTaskDesc):
+# 		self.taskDesc = str(newTaskDesc)
+
+
+# 	def printTask(self):
+# 		print(self.taskName + ": " + self.taskDesc)
+
+# 	def __str__(self):
+
+# 		return self.taskName + ": " + self.taskDesc
+
 class Task():
 
+    def __init__(self, taskName: str, taskDesc: str, priority: str = "Medium"):
+        self.taskName = taskName 
+        self.taskDesc = taskDesc 
+        self.priority = priority  # Added priority here
 
-	def __init__(self, taskName: str, taskDesc: str):
-		self.taskName = taskName 
-		self.taskDesc = taskDesc 
+    def updateTaskName(self, newTaskName: str):
+        self.taskName = newTaskName
 
-	def updateTaskName(self, newTaskName: str):
-		self.taskName = newTaskName
+    def updateTaskDesc(self, newTaskDesc):
+        self.taskDesc = str(newTaskDesc)
+        
+    def updatePriority(self, newPriority: str): #function to set priority
+        self.priority = newPriority
 
-	def updateTaskDesc(self, newTaskDesc):
-		self.taskDesc = str(newTaskDesc)
 
+    #Changed formatting for priority
+    def printTask(self):
+        print(f"{self.taskName}: {self.taskDesc} [{self.priority}]")
 
-	def printTask(self):
-		print(self.taskName + ": " + self.taskDesc)
-
-	def __str__(self):
-		return self.taskName + ": " + self.taskDesc
+    def __str__(self):
+        return f"{self.taskName}: {self.taskDesc} [{self.priority}]"
+    	
